f59b92958e2a9008ed7fad142b60bf9468cf0215
Nguyễn Tuấn Vinh <vinhtage@Vinhtage.local>
2025-04-04 17:29:48 +0700

    implement communicate class

--- include/communicate.hpp
+++ include/communicate.hpp
@@ -2,5 +2,31 @@
 #ifndef COMMUNICATE_HPP
 #define COMMUNICATE_HPP
 
+#include <Arduino.h>
+#include <WiFi.h>
+#include "esp_now.h"
+#include "base_module.hpp"
+
+struct Message
+{
+    char *content;
+};
+
+class Communicate : public BaseModule
+{
+private:
+    const uint8_t _PARTNER_MAC[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    Message _receiveMessage;
+    Message _sendMessage;
+
+    void onSendData(const uint8_t *mac_addr, esp_now_send_status_t status);
+    void onReceiveData(const uint8_t *mac, const uint8_t *incomingData, int len);
+
+    void taskFn() override;
+
+public:
+    Communicate();
+    ~Communicate();
+};
 
 #endif
--- src/communicate.cpp
+++ src/communicate.cpp
@@ -1,2 +1,22 @@
-#include "communicate.hpp" 
+#include "communicate.hpp"
 
+Communicate::Communicate()
+    : BaseModule("COMMUNICATE")
+{
+}
+
+Communicate::~Communicate() {}
+
+void Communicate::taskFn()
+{
+    esp_err_t result = esp_now_send(this->_PARTNER_MAC, (uint8_t *)&this->_sendMessage, sizeof(this->_sendMessage));
+
+    if (result == ESP_OK)
+    {
+        ESP_LOGI(this->NAME, "Sent with success");
+    }
+    else
+    {
+        ESP_LOGI(this->NAME, "Error sending the data");
+    }
+}
